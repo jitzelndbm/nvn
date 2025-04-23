d39ffa51caa6edd97a947ad51d6ee00fa46e69db
Jitze Lindeboom <jitze@linsoft.nl>
2025-04-23 17:12:29 +0200

    feat!: untested option and result types

---
+++ lua/nvn/option.lua
@@ -0,0 +1,290 @@
+---@class Result
+local Result = require("nvn.result");
+
+---@class Option
+---@field private _value any | nil
+local Option = {}
+Option.__index = Option
+
+---Returns an instance of the some variant of the option type.
+---
+---@nodiscard
+---@param value any
+---@return Option
+Option.Some = function(value)
+	local self = setmetatable({}, Option)
+	self._value = value;
+	return self
+end
+
+
+---Create a None variant of the Option class.
+---
+---@nodiscard
+---@return Option
+Option.None = function()
+	local self = setmetatable({}, Option)
+	self._value = nil;
+	return self
+end
+
+
+---If the function returns an error, this method will output the None variant
+---
+---@nodiscard
+---@param fn function
+---@param ... unknown
+---@return Option
+Option.pcall = function(fn, ...)
+	local ok, value = pcall(fn, ...);
+	if ok then
+		return Option.Some(value);
+	else
+		return Option.None()
+	end
+end
+
+
+---Returns None if the option is None, otherwise returns optb.
+---
+---@param optb any
+---@return any
+function Option:and_(optb)
+	if self:is_some() then
+		return optb
+	else
+		return Option.None()
+	end
+end
+
+---Returns None if the option is None, otherwise calls f with the wrapped value and returns the result.
+---
+---@nodiscard
+---@param f function
+---@return Option
+function Option:and_then(f)
+	if self:is_some() then
+		return Option.Some(f(self._value))
+	else
+		return Option.None()
+	end
+end
+
+---Returns the contained Some value, consuming the self value. Errors if the value is not present.
+---
+---@param msg string
+---@return any
+function Option:expect(msg)
+	if self:is_some() then
+		return self._value
+	else
+		error(msg, 2)
+	end
+end
+
+---Returns none if the option is none, otherwise call p, if that returns true ten return some variant, else none.
+---
+---@nodiscard
+---@param p any
+---@return Option
+function Option:filter(p)
+	if self:is_some() and p(self._value) then
+		return self
+	else
+		return Option.None()
+	end
+end
+
+---Converts from Option<Option<T>> to Option<T>.
+---
+---@nodiscard
+---@return Option
+function Option:flatten()
+	if self:is_some() then
+		return self
+	else
+		return Option.None()
+	end
+end
+
+---Mutate the value inside of the option
+---
+---@generic T
+---@param value T
+---@return T
+function Option:insert(value)
+	self._value = value
+	return value
+end
+
+---Calls a function with a reference to the contained value if Some.
+---
+---@nodiscard
+---@param f any
+---@return Option
+function Option:inspect(f)
+	if self:is_some() then
+		f(self._value)
+	end
+
+	return self
+end
+
+---Returns true if the option is not a value.
+---
+---@return boolean
+function Option:is_none()
+	return not self:is_some()
+end
+
+---Returns true  fi the option is not a value or the inside matches the predicate.
+---
+---@param f fun(x: any): boolean
+---@return boolean
+function Option:is_none_or(f)
+	return self:is_none() or f(self._value)
+end
+
+---Returns true if the option is a value.
+---
+---@return boolean
+function Option:is_some()
+	return self._value ~= nil
+end
+
+---Returns true if the option is a value and the inside matches the predicate.
+---
+---@param f fun(x: any): boolean
+---@return boolean
+function Option:is_some_and(f)
+	return self:is_some() and f(self._value)
+end
+
+---Maps an Option<T> to Option<U> by applying a function to a contained value (if Some) or returns None (if None).
+---
+---@nodiscard
+---@param f fun(x: any): any
+---@return Option
+function Option:map(f)
+	if self:is_some() then
+		return Option.Some(f(self._value))
+	else
+		return Option.None()
+	end
+end
+
+---Returns the provided default result (if none), or applies a function to the contained value (if any).
+---
+---@param default any
+---@param f fun(x: any): any
+---@return any
+function Option:map_or(default, f)
+	if self:is_some() then
+		return f(self._value)
+	else
+		return default
+	end
+end
+
+---Transforms the Option<T> into a Result<T, E>, mapping Some(v) to Ok(v) and None to Err(err).
+---
+---@nodiscard
+---@generic E
+---@param err E
+---@return Result
+function Option:ok_or(err)
+	if self:is_some() then
+		return Result.Ok(self._value)
+	else
+		return Result.Err(err)
+	end
+end
+
+---Transforms the Option<T> into a Result<T, E>, mapping Some(v) to Ok(v) and None to Err(err()).
+---
+---@nodiscard
+---@generic E
+---@param errf fun(): E
+---@return Result
+function Option:ok_or_else(errf)
+	if self:is_some() then
+		return Result.Ok(self._value)
+	else
+		return Result.Err(errf())
+	end
+end
+
+---Returns the option if it contains a value, otherwise returns optb.
+---
+---@nodiscard
+---@param optb Option
+---@return Option
+function Option:or_(optb)
+	if self:is_some() then
+		return self
+	else
+		return optb
+	end
+end
+
+---Returns the option if it contains a value, otherwise calls f and returns the result.
+---
+---@nodiscard
+---@param f fun(): Option
+---@return Option
+function Option:or_else(f)
+	if self:is_some() then
+		return self
+	else
+		-- Could be None as well, but will be automatically converted
+		return Option.Some(f())
+	end
+end
+
+---Replaces the actual value in the option by the value given in parameter, returning the old value if present, leaving a Some in its place without deinitializing either one.
+---
+---@nodiscard
+---@param value any
+---@return Option
+function Option:replace(value)
+	self._value = value
+	return self
+end
+
+---Returns the contained Some value, consuming the self value.
+---
+---@returns any
+function Option:unwrap()
+	if self:is_some() then
+		return self._value
+	else
+		error(self._value, 2)
+	end
+end
+
+---Returns the contained Some value or a provided default.
+---
+---@param default any
+---@return any
+function Option:unwrap_or(default)
+	if self:is_some() then
+		return self._value
+	else
+		return default
+	end
+end
+
+---Returns the contained Some value or computes it from a closure.
+---
+---@param f fun(): any
+---@return any
+function Option:unwrap_or_else(f)
+	if self:is_some() then
+		return self._value
+	else
+		return f()
+	end
+end
+
+
+return Option
---
+++ lua/nvn/result.lua
@@ -0,0 +1,290 @@
+---@class Option
+local Option = require("nvn.option")
+
+---@class Result
+---@field private _is_ok boolean
+---@field private _value any
+local Result = {}
+Result.__index = Result
+
+---Constructor for the Ok variant of Result
+---
+---@nodiscard
+---@param value any
+---@return Result
+Result.Ok = function(value)
+	local self = setmetatable({}, Result)
+	self._is_ok = true
+	self._value = value
+	return self
+end
+
+
+---Constructor for the Err variant of Result
+---
+---@nodiscard
+---@param error any
+---@return Result
+Result.Err = function(error)
+	local self = setmetatable({}, Result)
+	self._is_ok = false
+	self._value = error
+	return self
+end
+
+
+---Constructor that outputs an error type if the value is nil
+---
+---@nodiscard
+---@param value any | nil
+---@return Result
+Result.Option = function(value)
+	local self = setmetatable({}, Result)
+	if value then
+		self._is_ok = true
+	else
+		self._is_ok = false
+	end
+	self._value = value
+	return self
+end
+
+
+---Wraps pcall with proper error handling and overload signatures
+---
+---@nodiscard
+---@param fn function
+---@param err any
+---@param ... unknown
+---@return Result
+function Result.pcall(fn, err, ...)
+	local ok, value = pcall(fn, ...)
+	if ok then
+		return Result.Ok(value)
+	else
+		return Result.Err(err)
+	end
+end
+
+---Returns true if the result is Ok.
+---
+---@return boolean
+function Result:is_ok()
+	return self._is_ok
+end
+
+---Returns true if the result is Ok and the value inside of it matches a predicate.
+---
+---@param f fun(x: any): boolean
+---@return boolean
+function Result:is_ok_and(f)
+	-- Pass self._value into f, it is guaranteed that this is the Ok variant
+	return self:is_ok() and f(self._value)
+end
+
+--- Returns true if the result is Err
+---
+--- @return boolean
+function Result:is_err()
+	return not self._is_ok
+end
+
+---Returns true if the result is Err and the value inside of it matches a predicate.
+---
+---@param f fun(x: any): boolean
+---@return boolean
+function Result:is_err_and(f)
+	return self:is_err() and f(self._value)
+end
+
+---Transforms Result<T,E> into Option<T>
+---
+---@return Option
+function Result:ok()
+	if self:is_ok() then
+		return Option.Some(self._value)
+	else
+		return Option.None()
+	end
+end
+
+---Transforms Result<T,E> into Option<E>
+---
+---@return Option
+function Result:err()
+	if self:is_err() then
+		return Option.Some(self._value)
+	else
+		return Option.None()
+	end
+end
+
+---Maps a Result<T, E> to Result<U, E> by applying a function to a contained Ok value, leaving an Err value untouched.
+---
+---@nodiscard
+---@param op fun(x: any): any
+---@return Result
+function Result:map(op)
+	if self:is_ok() then
+		return Result.Ok(op(self._value))
+	else
+		return self
+	end
+end
+
+--- Returns the provided default (if Err), or applies a function to the contained value (if Ok).
+---
+---@param default any
+---@param f fun(ok: any): any
+---@return any
+function Result:map_or(default, f)
+	if self:is_ok() then
+		return f(self._value)
+	else
+		return default
+	end
+end
+
+--- Maps a Result<T, E> to U by applying fallback function default to a contained Err value, or function f to a contained Ok value.
+---
+---@param default fun(err: any): any
+---@param f fun(ok: any): any
+---@return any
+function Result:map_or_else(default, f)
+	if self:is_ok() then
+		return f(self._value)
+	else
+		return default(self._value)
+	end
+end
+
+--- Maps a Result<T, E> to Result<T, F> by applying a function to a contained Err value, leaving an Ok value untouched.
+---
+---@nodiscard
+---@param op fun(err: any)
+---@return Result
+function Result:map_err(op)
+	if self:is_ok() then
+		return self
+	else
+		return Result.Err(op(self._value))
+	end
+end
+
+---Returns the contained Ok value, consuming the self value. Panics if the value is an Err, with a panic message including the passed message, and the content of the Err.
+---
+---@nodiscard
+---@param msg string
+---@return any
+function Result:expect(msg)
+	if self:is_ok() then
+		return self._value
+	else
+		error(msg .. ": " .. self._value, 2)
+	end
+end
+
+---Returns the contained Ok value, consuming the self value. Panics if the value is an Err, with a panic message provided by the Err’s value.
+---
+---@return any
+function Result:unwrap()
+	if self:is_ok() then
+		return self._value
+	else
+		error(self._value, 2)
+	end
+end
+
+-- ---Returns the contained Ok value or a default
+-- ---
+-- ---@param default any
+-- ---@return any
+-- function Result:unwrap_or_default(default)
+-- 	if self:is_ok() then
+-- 		return self._value
+-- 	else
+-- 		return default
+-- 	end
+-- end
+
+-- TODO: expect_err
+-- TODO: unwrap_err
+
+
+---Returns res if the result is Ok, otherwise returns the Err value of self.
+---
+---@nodiscard
+---@param res Result
+---@return Result
+function Result:and_(res)
+	if self:is_ok() then
+		return res
+	else
+		return self
+	end
+end
+
+---Calls op if the result is Ok, otherwise returns the Err value of self.
+---
+---@nodiscard
+---@param op fun(ok: any): Result
+---@return Result
+function Result:and_then(op)
+	if self:is_ok() then
+		return op(self._value)
+	else
+		return self
+	end
+end
+
+---Returns res if the result is Err, otherwise returns the Ok value of self.
+---
+---@nodiscard
+---@param res Result
+---@return Result
+function Result:or_(res)
+	if self:is_ok() then
+		return self
+	else
+		return res
+	end
+end
+
+---Calls op if the result is Err, otherwise returns the Ok value of self.
+---
+---@nodiscard
+---@param op fun(err: any): Result
+---@return Result
+function Result:or_else(op)
+	if self:is_ok() then
+		return self
+	else
+		return op(self._value)
+	end
+end
+
+---Returns the contained Ok value or a provided default.
+---
+---@param default any
+---@return any
+function Result:unwrap_or(default)
+	if self:is_ok() then
+		return self._value
+	else
+		return default
+	end
+end
+
+---Returns the contained Ok value or computes it from a closure.
+---
+---@param op fun(err: any): any
+---@return any
+function Result:unwrap_or_else(op)
+	if self:is_ok() then
+		return self._value
+	else
+		return op(self._value)
+	end
+end
+
+return Result
